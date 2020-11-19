45babe51b1c823c0f2d62cac536ff8f0d36da1aa
sogyeongho <gso@student.42seoul.kr>
2020-11-20 00:11:14 +0900

    2020-11-20

---
+++ 문자열 내림차순으로 배치하기/src.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+
+char        *solution(const char *S)
+{
+    int     len = strlen(s) - 1;
+    char    temp;
+    char    *answer = (char *)malloc(sizeof(char) *(strlen(s) + 1));
+
+    strcpy(answer, s);
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len - i; j++)
+        {
+            if (answer[j] < answer[j + 1])
+            {
+                temp = answer[j];
+                answer[j] = answer[j + 1];
+                answer[j + 1] = temp;
+            }
+        }
+    }
+    return (answer);
+}
